aa0de1b589ded687a30fe2495470bdac62ae8618
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-04 04:33:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,6 +8,7 @@ def my_collect(array)
  empty_array
 end
 
-my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
-  name.split(" ").first
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
 end
