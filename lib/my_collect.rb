5221f370b0ff9c22bfcc74b7556ae73478cec713
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-04 04:35:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,10 @@
 def my_collect(collection)
   empty_array = []
   counter = 0
-  while counter < array.length
+  while counter < collection.length
     new_array << yield(collection[counter])
     counter += 1
   end
  empty_array
 end
 
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-my_collect(collection) do |lang|
-  lang.upcase
-end
