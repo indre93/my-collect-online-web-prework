f6f2f75988e450527f57208afd815644dcf2eca4
Indre Bakeer <indre.bakeer@gmail.com>
2019-01-04 04:36:02 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   empty_array = []
   counter = 0
   while counter < collection.length
-    new_array << yield(collection[counter])
+    empty_array << yield(collection[counter])
     counter += 1
   end
  empty_array
